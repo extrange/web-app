3b2ffca88764d9de63e61b25270ad9c6e6ef0abe
nicholaslyz <nicholaslyz@gmail.com>
2021-01-05 11:36:31 +0800

    update packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom';
 import {StrictMode} from "react";
-import 'fontsource-roboto/300.css'
-import 'fontsource-roboto/400.css'
-import 'fontsource-roboto/500.css'
-import 'fontsource-roboto/700.css'
-import 'fontsource-source-sans-pro' //For codemirror in markdownEditor.js
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/source-sans-pro' //For codemirror in markdownEditor.js
 import './components/starcraft.css' //Stylized font for menu
 import {MuiThemeProvider} from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
