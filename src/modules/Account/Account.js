206fb89863c0125ad6d7ed0a6b796785440ff4d1
nicholaslyz <nicholaslyz@gmail.com>
2021-04-29 13:31:41 +0800

    Fixed session endpoints and viewing

--- src/modules/Account/Account.js
+++ src/modules/Account/Account.js
@@ -3,24 +3,11 @@ import {useEffect, useState} from "react";
 import {Button, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography} from "@material-ui/core";
 import QRCode from 'qrcode.react'
 import {Networking} from "../../util/networking";
-import {getSavedBrowserDetail, SAVED_BROWSERS_URL, SESSION_MANAGEMENT_URL, TWOFACTOR_URL} from "./urls";
-import {formatDistanceToNowStrict, parseJSON} from "date-fns";
+import {BROWSERS_URL, getBrowserDetail, TWOFACTOR_URL} from "./urls";
+import {compareDesc, formatDistanceToNowStrict, parseJSON} from "date-fns";
 import {BackgroundScreen} from "../../shared/backgroundScreen";
 import CheckIcon from '@material-ui/icons/Check';
 import UAParser from 'ua-parser-js'
-import FingerprintJS from '@fingerprintjs/fingerprintjs'
-
-const fpPromise = FingerprintJS.load();
-
-(async () => {
-  // Get the visitor identifier when you need it.
-  const fp = await fpPromise
-  const result = await fp.get()
-
-  // This is the visitor identifier:
-  const visitorId = result.visitorId
-  console.log(visitorId)
-})()
 
 export const Account = ({returnToMainApp, logout}) => {
     const [drawerOpen, setDrawerOpen] = useState(false)
@@ -29,43 +16,31 @@ export const Account = ({returnToMainApp, logout}) => {
     const [otp, setOtp] = useState('')
     const [secret, setSecret] = useState()
 
-    const [sessions, setSessions] = useState([])
-    const [savedBrowsers, setSavedBrowsers] = useState([])
+    const [browsers, setDevices] = useState([])
 
     const prettifyUAString = uaString => {
         let ua = UAParser(uaString)
         let {
             browser: {
                 name: browserName,
-                version: browserVersion
             },
-            // if type is not set, it is mobile
-            device: {model, type, vendor},
+            // if type is set, it is mobile
+            device: {model, type},
             os: {name: osName, version: osVersion}
         } = ua
 
         return {
-            // prettified: `${browserName} ${browserVersion} on ${type ? `${model} (${osName} ${osVersion})` : `${osName} ${osVersion}`}`,
+            prettified: `${browserName} on ${osName} ${osVersion}${model ? ` (${model})` : ''}`,
             isMobile: Boolean(type),
-            prettified: uaString
         }
     }
 
-    const getBrowsers = () => Networking.send(SAVED_BROWSERS_URL)
-        .then(r => r.json())
-        .then(r => setSavedBrowsers(r))
-
-    const deleteBrowser = id => Networking.send(getSavedBrowserDetail(id), {method: Networking.DELETE})
+    const forgetAndLogoutBrowser = id => Networking.send(getBrowserDetail(id), {method: Networking.DELETE})
         .then(getBrowsers)
 
-    const getSessions = () => Networking.send(SESSION_MANAGEMENT_URL)
+    const getBrowsers = () => Networking.send(BROWSERS_URL)
         .then(r => r.json())
-        .then(r => setSessions(r))
-
-    const deleteSession = id => Networking.send(SESSION_MANAGEMENT_URL + `${id}/`, {
-        method: Networking.DELETE,
-    })
-        .then(getSessions)
+        .then(r => setDevices(r))
 
     const getOtpStatus = () => Networking.send(TWOFACTOR_URL)
         .then(r => r.json())
@@ -76,7 +51,6 @@ export const Account = ({returnToMainApp, logout}) => {
 
     useEffect(() => {
         void getOtpStatus()
-        void getSessions()
         void getBrowsers()
     }, [])
 
@@ -121,51 +95,58 @@ export const Account = ({returnToMainApp, logout}) => {
                 value={otp}
                 onChange={e => setOtp(e.target.value)}
             />
+            Active Browsers
             <Table>
                 <TableHead>
                     <TableRow>
                         <TableCell>IP</TableCell>
                         <TableCell>User-Agent</TableCell>
                         <TableCell>Last Activity</TableCell>
-                        <TableCell>Current Session</TableCell>
+                        <TableCell>This browser?</TableCell>
                         <TableCell>Country Code</TableCell>
-                        <TableCell>Logout</TableCell>
+                        <TableCell>Saved?</TableCell>
+                        <TableCell>Logout & Forget</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {sessions.map(e => <TableRow>
-                        <TableCell>{e.ip}</TableCell>
-                        <TableCell>{prettifyUAString(e.user_agent).prettified}</TableCell>
-                        <TableCell>{formatDistanceToNowStrict(parseJSON(e.last_activity), {addSuffix: true})}</TableCell>
-                        <TableCell>{e.is_current_session ? <CheckIcon/> : null}</TableCell>
-                        <TableCell>{e.country_code}</TableCell>
-                        <TableCell><Button variant={'contained'}
-                                           onClick={() => deleteSession(e.id)}>Logout</Button></TableCell>
-                    </TableRow>)}
+                    {browsers.filter(e => e.is_active).sort((a, b) => compareDesc(parseJSON(a), parseJSON(b))).map(e =>
+                        <TableRow key={e.id}>
+                            <TableCell>{e.ip}</TableCell>
+                            <TableCell>{prettifyUAString(e.user_agent).prettified}</TableCell>
+                            <TableCell>{formatDistanceToNowStrict(parseJSON(e.last_activity), {addSuffix: true})}</TableCell>
+                            <TableCell>{e.is_current_session ? <CheckIcon/> : null}</TableCell>
+                            <TableCell>{e.country_code}</TableCell>
+                            <TableCell>{e.is_saved ? <CheckIcon/> : null}</TableCell>
+                            <TableCell><Button variant={'contained'}
+                                               onClick={() => forgetAndLogoutBrowser(e.id)}>Logout</Button></TableCell>
+                        </TableRow>)}
                 </TableBody>
             </Table>
+            Inactive Browsers
             <Table>
                 <TableHead>
                     <TableRow>
+                        <TableCell>IP</TableCell>
+                        <TableCell>User-Agent</TableCell>
+                        <TableCell>Last Activity</TableCell>
+                        <TableCell>This browser?</TableCell>
                         <TableCell>Country Code</TableCell>
-                        <TableCell>User_agent</TableCell>
-                        <TableCell>IP last accessed</TableCell>
-                        <TableCell>last_login</TableCell>
-                        <TableCell>Is currentBrowser</TableCell>
+                        <TableCell>Saved?</TableCell>
                         <TableCell>Forget</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {savedBrowsers.map(e => <TableRow>
-                        <TableCell>{e.country_code}</TableCell>
-                        <TableCell>{prettifyUAString(e.user_agent).prettified}</TableCell>
-                        <TableCell>{e.ip_last_accessed}</TableCell>
-                        <TableCell>{formatDistanceToNowStrict(parseJSON(e.last_login), {addSuffix: true})}</TableCell>
-                        <TableCell>{e.is_current_browser ? <CheckIcon/> : null}</TableCell>
-                        <TableCell><Button variant={'contained'}
-                                           color={'secondary'}
-                                           onClick={() => deleteBrowser(e.id)}>Forget</Button></TableCell>
-                    </TableRow>)}
+                    {browsers.filter(e => !e.is_active).sort((a, b) => compareDesc(parseJSON(a.last_activity), parseJSON(b.last_activity))).map(e =>
+                        <TableRow key={e.id}>
+                            <TableCell>{e.ip}</TableCell>
+                            <TableCell>{prettifyUAString(e.user_agent).prettified}</TableCell>
+                            <TableCell>{formatDistanceToNowStrict(parseJSON(e.last_activity), {addSuffix: true})}</TableCell>
+                            <TableCell>{e.is_current_session ? <CheckIcon/> : null}</TableCell>
+                            <TableCell>{e.country_code}</TableCell>
+                            <TableCell>{e.is_saved ? <CheckIcon/> : null}</TableCell>
+                            <TableCell>{e.is_saved && <Button variant={'contained'}
+                                                              onClick={() => forgetAndLogoutBrowser(e.id)}>Forget</Button>}</TableCell>
+                        </TableRow>)}
                 </TableBody>
             </Table>
         </BackgroundScreen>
--- src/modules/Account/urls.js
+++ src/modules/Account/urls.js
@@ -2,6 +2,5 @@ import {API_URL} from "../../globals/urls";
 
 const ACCOUNT_URL = `${API_URL}/account`
 export const TWOFACTOR_URL = `${ACCOUNT_URL}/twofactor/`
-export const SESSION_MANAGEMENT_URL = `${ACCOUNT_URL}/sessions/`
-export const SAVED_BROWSERS_URL = `${ACCOUNT_URL}/saved-browsers/`
-export const getSavedBrowserDetail = id => `${SAVED_BROWSERS_URL}${id}/`
+export const BROWSERS_URL = `${ACCOUNT_URL}/browsers/`
+export const getBrowserDetail = id => `${BROWSERS_URL}${id}/`
