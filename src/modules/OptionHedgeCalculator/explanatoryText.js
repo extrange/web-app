fd3853450db3619b815f5e87193fb4ba925b8fc5
extrange <29305375+extrange@users.noreply.github.com>
2022-03-23 04:08:21 +0000

    Updated dependencies Increased number of stars on starfield

--- src/modules/OptionHedgeCalculator/explanatoryText.js
+++ src/modules/OptionHedgeCalculator/explanatoryText.js
@@ -1,5 +1,4 @@
-import marked from 'marked'
-
+import { marked } from "marked";
 
 export const explanatoryText = marked(`
 This calculates a 1:1 put spread (not the 1:1.1 as described in *The Options Edge*)
@@ -13,4 +12,4 @@ Rehedge in first month ONLY if SPY has moved > 3% from the previous month.
 | Beta of IWDP:     | 0.99                                                 |
 | Beta of IWDA:     | 0.89                                                 |
 | Portfolio Beta:   | 1 (since I'm only hedging CSPX and IWDA)             |
-`)
+`);
