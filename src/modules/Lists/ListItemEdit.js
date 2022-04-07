fac4f4ff6cafe45a5f8dad60081413e0dd7a843b
extrange <29305375+extrange@users.noreply.github.com>
2022-04-07 09:59:12 +0000

    Tidy due_date/repeat_days UI

--- src/modules/Lists/ListItemEdit.js
+++ src/modules/Lists/ListItemEdit.js
@@ -33,6 +33,10 @@ const Footer = styled.div`
   margin-left: 10px;
 `;
 
+const OptionsDiv = styled.div`
+  display: flex;
+`;
+
 const StyledTextField = styled(TextField)`
   margin: 5px 0;
 `;
@@ -131,29 +135,71 @@ export const ListItemEdit = ({
       open
       onClose={onClose}
       footer={
-        <Footer>
-          <Tooltip
-            arrow
-            enterTouchDelay={100}
-            interactive
-            title={
-              lastCreatedTimeString
-                ? `Created ${lastCreatedTimeString}`
-                : "Not yet created"
-            }
-          >
-            <Typography variant={"body2"} color={"textSecondary"}>
-              {lastSavedTimeString
-                ? `Last saved ${lastSavedTimeString}`
-                : "New item"}
-            </Typography>
-          </Tooltip>
-          <FooterRight>
-            <Button variant={"text"} color={"primary"} onClick={onClose}>
-              Close
-            </Button>
-          </FooterRight>
-        </Footer>
+        <>
+          <OptionsDiv>
+            <MuiPickersUtilsProvider utils={DateFns}>
+              <KeyboardDatePicker
+                autoOk
+                label={"Due"}
+                value={editingItem.due_date || null}
+                name={"due_date"}
+                onChange={(newVal) => {
+                  if (isValid(newVal) || newVal === null) {
+                    setValue(
+                      "due_date",
+                      newVal
+                        ? formatISO(newVal, { representation: "date" })
+                        : null
+                    );
+                    onChange(getValues());
+                    flush();
+                  }
+                }}
+                format={"dd/MM/yyyy"}
+                size={"small"}
+                placeholder={"dd/mm/yyyy"}
+                inputVariant={"outlined"}
+                style={{ margin: 5 }}
+              />
+            </MuiPickersUtilsProvider>
+            <TextField
+              label="Repeat Days"
+              name={"repeat_days"}
+              defaultValue={initialRepeatDays || ""}
+              type={"number"}
+              variant={"outlined"}
+              size={"small"}
+              onChange={(e) => {
+                setValue("repeat_days", e.target.value || 0);
+                onChange(getValues());
+              }}
+              style={{ margin: 5 }}
+            />
+          </OptionsDiv>
+          <Footer>
+            <Tooltip
+              arrow
+              enterTouchDelay={100}
+              interactive
+              title={
+                lastCreatedTimeString
+                  ? `Created ${lastCreatedTimeString}`
+                  : "Not yet created"
+              }
+            >
+              <Typography variant={"body2"} color={"textSecondary"}>
+                {lastSavedTimeString
+                  ? `Last saved ${lastSavedTimeString}`
+                  : "New item"}
+              </Typography>
+            </Tooltip>
+            <FooterRight>
+              <Button variant={"text"} color={"primary"} onClick={onClose}>
+                Close
+              </Button>
+            </FooterRight>
+          </Footer>
+        </>
       }
     >
       <StyledTextField
@@ -190,39 +236,6 @@ export const ListItemEdit = ({
           }}
         />
       )}
-      <MuiPickersUtilsProvider utils={DateFns}>
-        <KeyboardDatePicker
-          autoOk
-          value={editingItem.due_date || null}
-          name={"due_date"}
-          onChange={(newVal) => {
-            if (isValid(newVal) || newVal === null) {
-              setValue(
-                "due_date",
-                newVal ? formatISO(newVal, { representation: "date" }) : null
-              );
-              onChange(getValues());
-              flush();
-            }
-          }}
-          format={"dd/MM/yyyy"}
-          size={'small'}
-          placeholder={"dd/mm/yyyy"}
-          inputVariant={"outlined"}
-        />
-      </MuiPickersUtilsProvider>
-      <StyledTextField
-        label="Repeat Days"
-        name={"repeat_days"}
-        defaultValue={initialRepeatDays}
-        type={"number"}
-        variant={"outlined"}
-        size={"small"}
-        onChange={(e) => {
-          setValue("repeat_days", e.target.value);
-          onChange(getValues());
-        }}
-      />
     </DialogBlurResponsive>
   );
 };
